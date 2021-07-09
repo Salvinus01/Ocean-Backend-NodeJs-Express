8d262b2d8a525f3a986ba8bcd366c92802d237b7
Salvinus01 <86074085+Salvinus01@users.noreply.github.com>
2021-07-09 13:24:38 -0300

    Interação com MongoDB local

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ObjectId } = require("mongodb");
 
-//Indica que todo corpo de request está estruturado em JSON
-app.use(express.json());
+(async () => {
+  const url = "mongodb://localhost:27017";
+  const dbName = "ocean_bancodados_09_07_21";
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+  console.info("Conectando ao banco de dados...");
 
-/*
+  const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+
+  console.info("MongoDB conectado com sucesso!");
+
+  const db = client.db(dbName);
+
+  //Indica que todo corpo de request está estruturado em JSON
+  app.use(express.json());
+
+  app.get("/", function (req, res) {
+    res.send("Hello World");
+  });
+
+  /*
 Lista de Endpoints CRUD
 Create, Read (Single & All), Update, Delete
 Criar, Ler (Individual & Tudo), Atualizar, Remover
@@ -22,42 +35,45 @@ Quando uma aplicação segue os padrões REST, ela é chamada de RESTful
 [DELETE] -> Delete
 */
 
-const lista = ["Senhor dos Anéis", "Harry Potter"];
+  const lista = ["Senhor dos Anéis", "Harry Potter"];
+  const collFilmes = db.collection("filmes");
 
-// [GET] - Read all
-//  com função anônima
-app.get("/filmes", function (req, res) {
-  res.send(lista.filter(Boolean));
-});
+  // [GET] - Read all
+  //  com função anônima
+  app.get("/filmes", async function (req, res) {
+    const listaFilmes = await collFilmes.find().toArray();
+    res.send(listaFilmes);
+  });
 
-// [GET] - Read Single (ou Read by ID)
-//  exemplo com arrow function
-app.get("/filmes/:id", (req, res) => {
-  const id = req.params.id;
-  const item = lista[id - 1];
-  res.send(item);
-});
+  // [GET] - Read Single (ou Read by ID)
+  //  exemplo com arrow function
+  app.get("/filmes/:id", async (req, res) => {
+    const id = req.params.id;
+    const item = await collFilmes.findOne({ _id: ObjectId(id) });
+    res.send(item);
+  });
 
-// [POST] - Create
-app.post("/filmes", function (req, res) {
-  const item = req.body.nome;
-  lista.push(item);
-  res.send("item criado com sucesso");
-});
+  // [POST] - Create
+  app.post("/filmes", async function (req, res) {
+    const item = req.body;
+    await collFilmes.insertOne(item);
+    res.send(item);
+  });
 
-// [PUT] - Update
-app.put("/filmes/:id", function (req, res) {
-  const idFilme = req.params.id;
-  const nomeFilme = req.body.nome;
-  lista[idFilme - 1] = nomeFilme;
-  res.send("filme alterado com sucesso");
-});
+  // [PUT] - Update
+  app.put("/filmes/:id", async function (req, res) {
+    const idFilme = req.params.id;
+    const item = req.body;
+    await collFilmes.updateOne({ _id: ObjectId(idFilme) }, { $set: item });
+    res.send("filme alterado com sucesso");
+  });
 
-// [DELETE] - Delete
-app.delete("/filmes/:id", function (req, res) {
-  const idFilme = req.params.id;
-  delete lista[idFilme - 1];
-  res.send("filme excluido com sucesso");
-});
+  // [DELETE] - Delete
+  app.delete("/filmes/:id", async function (req, res) {
+    const idFilme = req.params.id;
+    await collFilmes.deleteOne({ _id: ObjectId(idFilme) });
+    res.send("filme excluido com sucesso");
+  });
 
-app.listen(3000);
+  app.listen(3000);
+})();
