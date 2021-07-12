39e97f85e8b8fad5269e8be62435369666e5d6a3
Luiz Gimenez Junior <86074085+Salvinus01@users.noreply.github.com>
2021-07-12 11:56:55 -0300

    Conexão com MongoDB Atlas

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const app = express();
 const { MongoClient, ObjectId } = require("mongodb");
 
 (async () => {
-  const url = "mongodb://localhost:27017";
-  const dbName = "ocean_bancodados_09_07_21";
+  //const url = "mongodb://localhost:27017";
+  const url = "mongodb+srv://admin:GVzwT2kZzR8XugnD@cluster0.ovq4e.mongodb.net/ocean_db?retryWrites=true&w=majority";
+  //const dbName = "ocean_bancodados_09_07_21";
+  const dbName = "ocean_db"
 
   console.info("Conectando ao banco de dados...");
 
-  //const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  const client = await MongoClient.connect(url, { useUnifiedTopology: true });
 
   console.info("MongoDB conectado com sucesso!");
 
-  //const db = client.db(dbName);
+  const db = client.db(dbName);
 
   //Indica que todo corpo de request está estruturado em JSON
   app.use(express.json());
@@ -41,8 +43,7 @@ Quando uma aplicação segue os padrões REST, ela é chamada de RESTful
   // [GET] - Read all
   //  com função anônima
   app.get("/filmes", async function (req, res) {
-    //const listaFilmes = await collFilmes.find().toArray();
-    const listaFilmes = undefined;
+    const listaFilmes = await collFilmes.find().toArray();
     res.send(listaFilmes);
   });
 
