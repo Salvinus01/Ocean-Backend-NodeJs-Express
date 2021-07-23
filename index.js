9eebd276b0e7fa8f716a064c88a039c646719fae
Luiz Gimenez Junior <86074085+Salvinus01@users.noreply.github.com>
2021-07-23 12:37:59 -0300

    Resolvendo problema de CORS

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/filmes/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
+app.get('/filmes', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
 app.listen(80, function () {
   console.log('CORS-enabled web server listening on port 80')
 })
