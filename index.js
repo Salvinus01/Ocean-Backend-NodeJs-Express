0a505758775643c8eaff3c90fd0a2951dafd4117
Luiz Gimenez Junior <86074085+Salvinus01@users.noreply.github.com>
2021-07-23 12:57:34 -0300

    Resolvendo CORS

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-var cors = require('cors')
 const { MongoClient, ObjectId } = require("mongodb");
 
 app.use(cors())
@@ -21,6 +20,22 @@ app.use(cors())
 
   //Indica que todo corpo de request está estruturado em JSON
   app.use(express.json());
+  
+  // CORS
+
+    app.all('/*', (req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+
+        res.header('Access-Control-Allow-Methods', '*');
+
+        res.header(
+            'Access-Control-Allow-Headers',
+            'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization',
+        );
+
+        next();
+    });
+
 
   app.get("/", function (req, res) {
     res.send("Hello World");
