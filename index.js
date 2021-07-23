d2b4c0e3b5288e5f9d85717d7402ed32a51dc6c5
Luiz Gimenez Junior <86074085+Salvinus01@users.noreply.github.com>
2021-07-23 12:36:03 -0300

    Resolvendo problema de CORS

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
+var cors = require('cors')
 const { MongoClient, ObjectId } = require("mongodb");
+
+app.use(cors())
+
+app.get('/filmes/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 (async () => {
   //const url = "mongodb://localhost:27017";
